e923e982e12c2edda7987e6a92e9d7a4897074cf
safonov-andrey <safonov.andrey.eho@gmail.com>
2021-01-28 20:47:34 +0200

    Player control

--- MetalInMotion/Source/MetalInMotion/Private/BallBearing.h
+++ MetalInMotion/Source/MetalInMotion/Private/BallBearing.h
@@ -1,32 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/**
+
+Main ball bearing implementation.
+
+Original author: Rob Baker.
+Current maintainer: Rob Baker.
+
+*********************************************************************************/
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Components/StaticMeshComponent.h"
 #include "BallBearing.generated.h"
 
+
+/**
+Main ball bearing class, derived from pawn but with no input and no camera.
+*********************************************************************************/
+
 UCLASS()
-class ABallBearing : public APawn
+class METALINMOTION_API ABallBearing : public APawn
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
+	
+	// Create a static mesh for this ball bearing on object construction.
 	ABallBearing();
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=BallBearing)
-	UStaticMeshComponent* BallMesh;
+	// The static mesh that represents the ball bearing.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = BallBearing)
+		UStaticMeshComponent* BallMesh = nullptr;
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	// Is the ball bearing attractive to magnets?
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = BallBearing)
+		bool Magnetized = true;
 };
